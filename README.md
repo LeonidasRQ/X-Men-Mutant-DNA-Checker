01ebbbaddde74acaf8f88ce4e3e306e28a55fb36
Leonidas <leonidas.ramirez.q@gmail.com>
2022-05-17 00:19:34 -0500

    implement get Stats function

---
+++ README.md
@@ -0,0 +1,9 @@
+# X-Men Project: Mutant Checker API
+
+## Description
+
+This project was created for helping Magneto to recruit new mutants for fighting the X-men. It will allow Magneto the analyze the candidate's DNA for checking if its a mutant of not.
+
+## Run In Postman
+
+[![Run in Postman](https://run.pstmn.io/button.svg)](https://app.getpostman.com/run-collection/14464237-a52913c0-1333-478f-b92f-3fe6666ea3ac?action=collection%2Ffork&collection-url=entityId%3D14464237-a52913c0-1333-478f-b92f-3fe6666ea3ac%26entityType%3Dcollection%26workspaceId%3D5751af16-cc8d-4e96-965a-e20d506d6a8d)
--- src/getStats.js
+++ src/getStats.js
@@ -4,17 +4,39 @@ async function getStats(event) {
   try {
     const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-    const result = await dynamodb
+    const count_mutant_dna = await dynamodb
       .scan({
         TableName: "DnaTable",
+        FilterExpression: "isMutant = :v1",
+        ExpressionAttributeValues: {
+          ":v1": true,
+        },
+        Select: "COUNT",
       })
       .promise();
 
-    const stats = result.Items;
+    const count_human_dna = await dynamodb
+      .scan({
+        TableName: "DnaTable",
+        FilterExpression: "isMutant = :v1",
+        ExpressionAttributeValues: {
+          ":v1": false,
+        },
+        Select: "COUNT",
+      })
+      .promise();
+
+    const ratio = count_mutant_dna.Count / count_human_dna.Count;
+
+    const response = {
+      count_mutant_dna,
+      count_human_dna,
+      ratio,
+    };
 
     return {
       status: 200,
-      body: { stats },
+      body: JSON.stringify(response),
     };
   } catch (error) {
     console.log(error);
